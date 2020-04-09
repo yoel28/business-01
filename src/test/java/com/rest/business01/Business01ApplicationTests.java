a54b98b2b8144dae060d5e40e94351d49036a633
Yoel Rodriguez <yo3l.m18@gmail.com>
2020-04-09 15:58:08 -0500

    init

---
+++ src/test/java/com/rest/business01/Business01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.business01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Business01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
